a015cd9007692d936b2fadbc27bc56c13a20eb07
Am1d1v <dimadiman1adima@gmail.com>
2024-07-28 13:25:11 +0300

    Cypress Library HTTP Response Mock

--- cypress/e2e/Mock.cy.js
+++ cypress/e2e/Mock.cy.js
@@ -2,22 +2,27 @@
 
 describe('Cypress Mock', () => {
 
-  it('Library mock', () => {
+  it('should return list with only one book and display specific message', () => {
     cy.visit('https://rahulshettyacademy.com/angularAppdemo');
-    cy.contains('Library').click();
 
     cy.intercept({
       method: 'GET',
       url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
     }, {
       statusCode: 200,
-      body: {
+      body: [{
         "book_name": 'RestAssured with Java',
         "isbn": "DSR",
         "aisle": "2304"
-      }
-    });
+      }]
+    }).as('boookRetrievals');
 
+    cy.contains('Library').click();
+
+    cy.wait('@boookRetrievals');
+
+    cy.get('tbody').should('have.length', 1);
+    cy.contains('Oops only 1 Book available');
 
   });
 
