6a31b9708c284c005747a634ccb0601fbf6b93a8
Am1d1v <dimadiman1adima@gmail.com>
2024-07-28 13:15:23 +0300

    Cypress HTTP Response Mock

--- cypress/e2e/Mock.cy.js
+++ cypress/e2e/Mock.cy.js
@@ -1,9 +1,25 @@
+///<reference types="cypress"/>
+
+describe('Cypress Mock', () => {
+
+  it('Library mock', () => {
+    cy.visit('https://rahulshettyacademy.com/angularAppdemo');
+    cy.contains('Library').click();
+
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+    }, {
+      statusCode: 200,
+      body: {
+        "book_name": 'RestAssured with Java',
+        "isbn": "DSR",
+        "aisle": "2304"
+      }
+    });
 
 
-describe('template spec', () => {
+  });
 
-  it('passes', () => {
-    cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
-  })
 
 })
