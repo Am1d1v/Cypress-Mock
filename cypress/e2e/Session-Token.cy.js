ed4d84015519a41c50306841c1451b226e93d1eb
Am1d1v <dimadiman1adima@gmail.com>
2024-07-29 17:11:04 +0300

    JWT Session Token. Certain product purchasing

--- cypress/e2e/Session-Token.cy.js
+++ cypress/e2e/Session-Token.cy.js
@@ -9,7 +9,7 @@ describe('JWT Session Token', () => {
     });
 
 
-    it.only('should log in through local storage using visit extra options', () => {
+    it('should log in through local storage using visit extra options', () => {
         // Session token injection into local storage
         cy.JWTLogin().then(() => {
 
@@ -18,6 +18,43 @@ describe('JWT Session Token', () => {
                     window.localStorage.setItem('token', Cypress.env('token'));
                 } 
             });
+
+        });
+
+    });
+
+    it.only('should log in through local storage and purchase certain item', () => {
+        // Session token injection into local storage
+        cy.JWTLogin().then(() => {
+
+        // Add certain product to the cart
+        cy.get(':nth-child(2) > .card > .card-body > .w-10').click();
+
+        // Only 1 item in the cart
+        cy.get(':nth-child(4) > .btn').contains(1);
+
+        // Move to the cart
+        cy.get(':nth-child(4) > .btn').click();
+
+        // Click on 'Buy Now' button
+        cy.get('.removeWrap > .btn-primary').click();
+
+        // Insert in Personal Information input fields dummy data
+        // Insert CVV Code
+        cy.get(':nth-child(2) > :nth-child(2) > .input').type(123);
+
+        // Insert Name on Card
+        cy.get(':nth-child(3) > .field > .input').type('Customer Name');
+
+        // Insert Country name
+        cy.get('.form-group > .input').type('Australia');
+        cy.get('.ta-item').click();
+
+        // Click on 'Place Order' button
+        cy.get('.btnn').click();
+
+        // Item was successfully purchased
+        cy.contains('Thankyou for the order.');
 
         });
 
